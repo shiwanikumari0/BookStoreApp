af41a49fcc78600b32a3576013dacd5eee6dbef9
Shiwani <001shiwanikumari10@gmail.com>
2024-04-19 04:27:03 +0530

    Backend Done!!

--- Backend/index.js
+++ Backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cors from "cors";
 
 import bookRoute from "./route/book.route.js"
-const app = express()
+import userRoute from "./route/user.route.js";
+const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 dotenv.config();
 
@@ -25,6 +30,7 @@ try {
 
 // defining routes
 app.use("/book", bookRoute);
+app.use("/user", userRoute);
 
 
 app.listen(PORT, () => {
